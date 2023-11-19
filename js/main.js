5a6efc0f3dc3ce90fb47dc6bec8e89f0dbc5cf99
Josh Fung <joshuagyfung@gmail.com>
2023-11-18 20:04:52 -0800

    Added skeleton search input and pick for me button

--- js/main.js
+++ js/main.js
@@ -1,32 +1,46 @@
-d3.csv('data/lego_data.csv').then(data => {
-
+d3.csv("data/lego_data.csv").then((data) => {
     // Convert columns to numerical values
-    data.forEach(d => {
-
+    data.forEach((d) => {
         d.set_year = +d.set_year;
         d.theme_id = +d.theme_id;
         d.set_num_parts = +d.set_num_parts;
         d.color_id = +d.color_id;
+    });
 
-    })
-
-    const dispatcher = d3.dispatch('selectedPoint');
+    const dispatcher = d3.dispatch(
+        "searchPoint",
+        "randomPoint",
+        "selectedPoint"
+    );
     const filteredData = {};
 
-    data.forEach(d => {
+    data.forEach((d) => {
         const setNum = d.set_num;
         if (!filteredData[setNum]) {
             filteredData[setNum] = {
-                theme_name: d.theme_name
+                theme_name: d.theme_name,
             };
         }
     });
 
+    d3.select("#search-input").on("keypress", (event) => {
+        if (event.key === "Enter") {
+            // TODO: Dispatch search event
+            console.log(`Search input entered for: ${event.target.value}`);
+            event.target.value = "";
+        }
+    });
 
+    d3.select("#pick-random").on("click", () => {
+        // TODO: Dispatch random selection event
+        console.log("Pick random button clicked!");
+    });
 
-
-    const networkGraph = new NetworkGraph({
-        parentElement: d3.select('#network-graph-container'),
-    }, dispatcher, filteredData);
-
+    const networkGraph = new NetworkGraph(
+        {
+            parentElement: d3.select("#network-graph-container"),
+        },
+        dispatcher,
+        filteredData
+    );
 });
