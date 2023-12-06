e1a45f0eb11a399f78b1d872e68b2417ac1be2ae
Josh Fung <joshuagyfung@gmail.com>
2023-12-05 16:04:41 -0800

    Still progressing on shifting points

--- main.py
+++ main.py
@@ -5,36 +5,6 @@ import numpy as np
 def drop_first_x_cols(df, num_cols):
     return df.iloc[:, num_cols:]
 
-# def adjust_points(df, theme, threshold, max_attempts):
-#     def manhattan_distance(row1, row2):
-#         return abs(row1['x'] - row2['x']) + abs(row1['y'] - row2['y'])
-#
-#     attempts = 0
-#     while attempts < max_attempts:
-#         subset = df[df['theme_name'] == theme]
-#         pairs_to_adjust = []
-#
-#         # Identify pairs that need adjustment
-#         for i, row1 in subset.iterrows():
-#             for j, row2 in subset.iterrows():
-#                 if i < j and manhattan_distance(row1, row2) <= threshold:
-#                     pairs_to_adjust.append((i, j))
-#
-#         # Report the number of pairs identified
-#         print(f"Attempt {attempts + 1}: {len(pairs_to_adjust)} pairs identified")
-#
-#         # Adjust points
-#         for i, j in pairs_to_adjust:
-#             # Randomly choose a point to adjust
-#             point_to_adjust = i if np.random.random() > 0.5 else j
-#             # Shift the point slightly
-#             df.at[point_to_adjust, 'x'] += np.random.uniform(-2, 2)
-#             df.at[point_to_adjust, 'y'] += np.random.uniform(-2, 2)
-#
-#         attempts += 1
-#
-#     return df
-
 def adjust_points(df, theme, threshold, max_attempts):
     def manhattan_distance(row1, row2):
         return abs(row1['x'] - row2['x']) + abs(row1['y'] - row2['y'])
@@ -66,7 +36,13 @@ def adjust_points(df, theme, threshold, max_attempts):
         # Adjust points
         for i, j in pairs_to_adjust:
             point_to_adjust = i if np.random.random() > 0.5 else j
+
+            # TODO: CHANGE HOW MUCH TO ADJUST POINTS
+            # How much to adjust x by -> Random value between the first and second value
             adjustment_x = np.random.uniform(-2, 2)
+
+            # TODO: CHANGE HOW MUCH TO ADJUST POINTS
+            # How much to adjust y by -> Random value between the first and second value
             adjustment_y = np.random.uniform(-2, 2)
             adjustments[point_to_adjust] = (adjustment_x, adjustment_y)
 
@@ -95,14 +71,14 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    version = 22
+    version = 25
 
     # Load your dataset
     file_path = f"data/hexagon_data_with_coordinates_v{version}.csv"  # Replace with your file path
     df = pd.read_csv(file_path)
 
     # Adjust points in the dataset
-    adjusted_df = adjust_points(df, 'Star Wars', 7, 75)
+    adjusted_df = adjust_points(df, 'Ninjago', 7.5, 100)
     # adjusted_df = adjust_points(adjusted_df, 'Ninjago', 7, 75)
     # adjusted_df = adjust_points(adjusted_df, 'Friends', 7, 75)
 
